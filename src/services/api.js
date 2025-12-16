1954fa4ec47d99348ed95705f9ffe14f4399ed5c
rinkaltailor8-stack <rinkaltailor8@gmail.com>
2025-12-16 22:43:04 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'http://rent-manager-api-blond.vercel.app/';
 
 const api = axios.create({
   baseURL: API_URL,
