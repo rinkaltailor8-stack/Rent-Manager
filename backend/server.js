fc5d5003ea320b2d6a165740de193188ee939e6c
rinkaltailor8-stack <rinkaltailor8@gmail.com>
2025-12-17 16:55:41 +0530

    fixed API issue

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
